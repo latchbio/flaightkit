d0ebdc6728490f580be0f59293b0479853f8cfde
Kenny Workman <kennyworkman@sbcglobal.net>
2021-09-20 16:21:48 -0700

    fix: python value conversion

--- flytekit/core/type_engine.py
+++ flytekit/core/type_engine.py
@@ -269,14 +269,14 @@ class UnionTransformer(TypeTransformer[typing.Union[typing.Any]]):
 
     def to_python_value(self, ctx: FlyteContext, lv: Literal, expected_python_type: Type[T]) -> T:
         for x in expected_python_type.__args__:
-            if x == type(None):
-                continue
             try:
                 res = TypeEngine.to_python_value(ctx, lv, x)
-                if res is None and x != type(None):
-                    continue
 
-                return res
+                if (res is None and lv is None and x == type(None)) or (
+                    res is not None and x != type(None)
+                ):
+                    return res
+
             except AssertionError:
                 pass
 
